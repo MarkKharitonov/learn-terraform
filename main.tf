9d314b50089d1b571a4fb453961143cb668d6d3e
Mark Kharitonov <mark.kharitonov@ceridian.com>
2019-12-10 19:15:08 -0500

    Use S3 backend

--- main.tf
+++ main.tf
@@ -2,98 +2,54 @@ provider "aws" {
   region = "us-east-2"
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "mark-kharitonov-terraform-up-and-running-state"
 
-output "clb_dns_name" {
-  value       = aws_elb.example.dns_name
-  description = "The domain name of the load balancer"
-}
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
 
-data "aws_availability_zones" "all" {}
-
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
-  lifecycle {
-    create_before_destroy = true
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.id
-  availability_zones   = data.aws_availability_zones.all.names
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-  min_size = 3
-  max_size = 10
-  
-  load_balancers    = [aws_elb.example.name]
-  health_check_type = "ELB"
-  
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
 
-resource "aws_elb" "example" {
-  name               = "terraform-asg-example"
-  security_groups    = [aws_security_group.elb.id]
-  availability_zones = data.aws_availability_zones.all.names
-
-  health_check {
-    target              = "HTTP:${var.server_port}/"
-    interval            = 30
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  # This adds a listener for incoming HTTP requests.
-  listener {
-    lb_port           = 80
-    lb_protocol       = "http"
-    instance_port     = var.server_port
-    instance_protocol = "http"
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "mark-kharitonov-terraform-up-and-running-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
   }
 }
 
-resource "aws_security_group" "elb" {
-  name = "terraform-example-elb"
-
-  # Allow all outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Inbound HTTP from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
