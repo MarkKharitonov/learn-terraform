db14e5ae0051bd0ef7b4b991cef06fb012234225
Mark Kharitonov <mark.kharitonov@ceridian.com>
2019-12-11 23:51:47 -0500

    Use AWS autoscaling group with isolation through dedicated state files

--- stage/services/webserver-cluster/outputs.tf
+++ stage/services/webserver-cluster/outputs.tf
@@ -1,14 +1,4 @@
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the web server"
-}
-
-output "public_dns" {
-  value       = aws_instance.example.public_dns
-  description = "The public domain name of the web server"
-}
-
-output "user_data" {
-  value       = aws_instance.example.user_data
-  description = "The user data of the web server"
+output "clb_dns_name" {
+  value       = aws_elb.example.dns_name
+  description = "The domain name of the load balancer"
 }
