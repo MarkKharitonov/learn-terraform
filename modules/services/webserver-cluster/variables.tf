c1c15ca133765759157973f5842a0bbad21e3540
Mark Kharitonov <mark.kharitonov@ceridian.com>
2020-01-02 21:17:03 -0500

    Demo the for_each keyword

--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -26,3 +26,9 @@ variable "max_size" {
   type        = number
   default     = 10
 }
+
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
